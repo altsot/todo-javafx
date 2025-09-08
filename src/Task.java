ad879413ea8da20bdc59e31e7ba3741e625a52f5
altsot <altsot@localhost.localdomain>
2025-09-08 10:33:17 +0700

    Create Task.java

--- src/Task.java
+++ src/Task.java
@@ -1,5 +1,25 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Task {
+    private String name;
+    private boolean completed;
+
+    public Task(String name) {
+        this.name = name;
+        this.completed = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
     }
 }
